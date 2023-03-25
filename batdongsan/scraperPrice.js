9095251d6bfd20591d6accfa6211f01bb8662ff0
vmh <vmhpublic2017@gmail.com>
2023-03-25 10:39:43 +0700

    batdongsan

--- batdongsan/scraperPrice.js
+++ batdongsan/scraperPrice.js
@@ -5,13 +5,24 @@ const scrapeCategory = (browser, url) =>
       console.log(">> Open new page ...");
       await pageInfo.goto(url);
       console.log(">> Accessing " + url);
-      await pageInfo.waitForSelector("body");
+      await pageInfo.waitForSelector("#boxSearchForm");
       console.log(">> Page load done...");
+
+      const headerData = await pageInfo.$eval(
+        "body > div.re__main > div > div.re__main-content",
+        (el) => {
+          return {
+            title: el.querySelector("h1.re__srp-title").innerText,
+          };
+        }
+      );
+
+      console.log("headerData", headerData);
 
       const dataItem = await pageInfo.$$eval("#product-lists-web", (els) => {
         dataItem = els.map((el) => {
           return {
-            title: el.querySelector("div.js__card > a").innerText,
+            title: el.querySelector("div.js__card > a").title,
             link: el.querySelector("div.js__card > a").href,
           };
         });
@@ -22,10 +33,11 @@ const scrapeCategory = (browser, url) =>
 
       await pageInfo.close();
       console.log(">> Tab đã đóng...");
-
       resolve(dataItem);
     } catch (error) {
       console.log("Controller failed: " + error);
+      await pageInfo.close();
+      console.log(">> Tab đã đóng...");
       reject(error);
     }
   });
